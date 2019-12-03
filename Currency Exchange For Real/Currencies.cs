60712c1ccf124afd11698be9c03a7f561d20a2ff
DieLabtec <55961068+DieLabtec@users.noreply.github.com>
2019-12-03 15:41:44 +0200

    Add files via upload

---
+++ Currency Exchange For Real/Currencies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency_Exchange_For_Real
+{
+    public class Currencies
+    {
+        public string Base;
+        public DateTime date;
+        public List<string> rates;
+      
+
+    }
+}
